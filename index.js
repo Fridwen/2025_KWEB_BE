ba17342aeefd3e6f1ac2a009ccc64be6c661bb21
chinmok4ban5ban <fridwen@gmail.com>
2025-04-30 12:27:37 +0900

    Exercise 3.4

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express');
-const { isInteger } = require('lodash');
 const app = express();
 const port = 3000;
 
-function facto(n) {
-  if (isInteger(n) === false || n < 0) return null;
-  if (n === 0 || n === 1) return 1;
-  let result = 1;
-  for (let i = 2;i <= n;i++) {
-    result *= i;
-  }
-  return result;
-}
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/factorial', (req, res) => {
-  const { factorial } = req.query;
-  res.redirect(`/factorial/${factorial}`)
+app.get('/', (req, res) => {
+  res.send(`<form method="post" action="/login">
+    <div>
+    <label>Username:</label>
+    <input id="username-input" name="username" type="text">
+    </div>
+    <div>
+    <label>Password:</label>
+    <input id="password-input" name="password" type="password">
+    </div>
+    <div>
+    <div>Introduce yourself</div>
+    <textarea id="introduction-input" name="introduction"></textarea>
+    </div>
+    <button type="submit">Submit</button>
+    </form>`)
   });
-app.get('/factorial/:number', (req, res) => res.send(`${req.params.number}! is ${facto(Number(req.params.number))}`));
+app.post('/login', (req, res) => {
+  const { username, password, introduction } = req.body;
+  res.send(`${username}'s password is ${password}`);
+  console.log(introduction);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
